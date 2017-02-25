1cbdcb77a9a5f5e4d8c9c135f51aad3ea580bca8
Enrico Borba <enricozb@gmail.com>
2017-02-24 19:46:36 -0800

    Rhyme completed and 20 poems generated.

--- code/miniproject2.py
+++ code/miniproject2.py
@@ -6,7 +6,7 @@ import random
 
 distribution = [5] * 2 + [6] * 15 + [7] * 41 + [8] * 70 + [9] * 63 + [10] * 25
 
-def generate_poem(reverse=False):
+def generate_poems(reverse=False, n_poems=1):
     lines, word_map = get_shakespeare(mapping='id', reverse=reverse)
     print('Training')
     model = unsupervised_HMM(lines, 6, 10)
@@ -14,28 +14,33 @@ def generate_poem(reverse=False):
     print('Emitting')
     print()
 
-    poem = []
-    for p in range(7):
-        pair = []
-        for end in get_rhyme_pair():
-            initial_obs = [word_map[end]]
-            num_words = random.choice(distribution)
-            words = list(map(word_map.get, model.generate_emission(num_words, initial_obs)))
-            while sum(map(count_syllables, words)) != 10:
+    for _ in range(n_poems):
+        lines = []
+        for p in range(7):
+            for end in get_rhyme_pair():
+                initial_obs = [word_map[end]]
+                num_words = random.choice(distribution) - 1
                 words = list(map(word_map.get, model.generate_emission(num_words, initial_obs)))
-            words.reverse()
-            pair.append(words)
-        poem.append(pair)
-    return poem
+                count = 0
+                while sum(map(count_syllables, words)) != 10:
+                    if count >= 1000:
+                        num_words = random.choice(distribution) - 1
+                        count = 0
+                    words = list(map(word_map.get, model.generate_emission(num_words, initial_obs)))
+                    count += 1
+                words.reverse()
+                lines.append(words)
+        poem = [lines[i] for i in [0, 2, 1, 3, 4, 6, 5, 7, 8, 10, 9, 11, 12, 13]]
+        yield poem
 
 def reform(lst):
     lst = ['I' if w == 'i' else w for w in lst]
     lst[0] = lst[0].capitalize()
     return lst
 
-poem = generate_poem()
-for p in poem:
-    print(p)
-# poem = list(map(reform, poem))
-# for i, line in enumerate(poem):
-#     print('{i:<2} {line}'.format(i=i + 1, line=' '.join(line)))
+for poem_i, poem in enumerate(generate_poems(n_poems=154)):
+    poem = list(map(reform, poem))
+    print('\n\n' + ' ' * 19 + str(poem_i + 1))
+    for i, line in enumerate(poem):
+        print('{i:<2} {line}'.format(i=i + 1, line=' '.join(line)))
+    print()
